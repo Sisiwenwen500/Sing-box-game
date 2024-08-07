fdebd1d750d7f7c65377e2cc58761a79388284c4
Sisiwenwen500 <151725452+Sisiwenwen500@users.noreply.github.com>
2024-08-08 03:57:07 +0800

    testsingbox.sh

--- testsingbox.sh
+++ testsingbox.sh
@@ -458,7 +458,7 @@ menu() {
     case "${choice}" in
         1) install_singbox ;;
         2) uninstall_singbox ;; 
-        3) cat "$WORKDIR/list.txt"
+        3) cat "$WORKDIR/list.txt" ;;
         0) exit 0 ;;
         *) red "无效的选项，请输入 0 到 3" ;;
    esac
